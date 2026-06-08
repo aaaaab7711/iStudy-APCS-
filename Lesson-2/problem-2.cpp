52ffefb633e1e79784c9fa9b041c9b1199499b9c
王阿斌 <wangabin@wangabindeMacBook-Pro.local>
2026-06-08 14:36:25 +0800

    20260608

---
+++ Lesson-2/problem-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << " /\\_/\\" << endl;
+    cout << "( o.o )" << endl;
+    cout << " > ^ <" << endl;
+
+    return 0;
+}
+
+//  /\_/\\
+// ( >w< )
+//  /   \
